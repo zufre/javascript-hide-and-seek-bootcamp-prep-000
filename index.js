667223f249694a6686edb86af4a0bffc49b6e2a8
Cedric David <cedric.david@btopenworld.com>
2018-08-27 01:46:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-function getFirstSelector(selector){
-  return document.querySelector(selector);
+function getFirstSelector(selector) {
+   return document.querySelector(selector);
 }
-function nestedTarget(){
-return document.querySelector('#nested .target')
-}
-function  increaseRankBy(n){
-  const rankedLists = document.querySelectorAll('.ranked-list')
-  const firstList = rankedLists[0]
-  const secondList = rankedLists[1]
-  let children1 = firstList.children
 
-  for (let i = 0, l = children1.length; i < l; i++) {
-    children1[i].innerHTML=(parseInt(children1[i].innerHTML) + n)
+function nestedTarget() {
+ return document.querySelector('#nested div div div.target');
+}
+
+function increaseRankBy(n) {
+
+const list =  document.querySelectorAll('.ranked-list li') ;
+
+for (let i = 0; i< list.length; i++){
+  list[i].innerHTML = (parseInt(list[i].innerHTML) + n).toString();
+}
+
+
+
+
+ }
+
+function deepestChild() {
+  let list1 = document.querySelector('#grand-node');
+  let list2 = list1.children[0];
+  while (list2) {
+    list1 = list2;
+    list2 = list1.children[0];
   }
-  let children2 = secondList.children
-
-
-  for (let i = 0, l = children2.length; i < l; i++) {
-children2[i].innerHTML=(parseInt(children2[i].innerHTML)+ n)
-  }
-}
-function deepestChild(){
-  return document.querySelector( '#grand-node div div div div')
-
-
+  return list1;
+  
 
 }
